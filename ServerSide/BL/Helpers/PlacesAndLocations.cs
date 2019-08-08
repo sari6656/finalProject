f8cb888b3ed36493c2f28aec91bee6ae5c0e401b
hitmachut <hitmachut@students.local>
2019-08-08 12:42:07 +0300

    add location functions

--- ServerSide/BL/Helpers/PlacesAndLocations.cs
+++ ServerSide/BL/Helpers/PlacesAndLocations.cs
@@ -24,5 +24,35 @@ namespace BL.Helpers
                 Longitude = shop.longitude
             };
         }
+        //פונקציית עזר לפונקציה המחזירה מרחק
+        private static double rad(double x)
+        {
+            return x * Math.PI / 180;
+        }
+        // הפונקציה מחזירה מרחק בין שני מיקומים במטרים
+        public static double getDistance(double p1Lat, double p1Long, double p2Lat, double p2Long)
+        {
+            int R = 6378137;// Earth’s mean radius in meter
+            double dLat = rad(p2Lat - p1Lat);
+            double dLong = rad(p2Long - p1Long);
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                Math.Cos(rad(p1Lat)) * Math.Cos(rad(p2Lat)) *
+                Math.Sin(dLong / 2) * Math.Sin(dLong / 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            double d = R * c;
+            return d;// returns the distance in meter
+        }
+
+        //פונקציה זו, כל 6 שניות מדפיסה את התאריך, כנראה שלא יהיה נצרך
+        public static void Timer()
+        {
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromMinutes(0.1);
+
+            var timer = new System.Threading.Timer((e) =>
+            {
+                Console.WriteLine(DateTime.Now);
+            }, null, startTimeSpan, periodTimeSpan);
+        }
     }
 }
